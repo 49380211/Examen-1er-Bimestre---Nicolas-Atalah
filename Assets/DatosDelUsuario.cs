8d594fa99c01b37dfd19c026215ff4de8bfac903
ATALAH WAISBERG NICOLAS MARTIN <49380211@ort>
2025-04-29 08:40:39 -0300

    terminado2

--- Assets/DatosDelUsuario.cs
+++ Assets/DatosDelUsuario.cs
@@ -9,37 +9,59 @@ public class DatosDelUsuario : MonoBehaviour
     public int kilometros;
     public int VTV;
     public int HC;
-
     // Start is called before the first frame update
     void Start()
     {
         if (patente == "")
         {
             Debug.Log("Porfavor ingrese su patente");
+            Debug.Log("VTV no aprobada");
+
             return;
         }
 
         if (modelo < 1900 || modelo > 2025)
         {
             Debug.Log("Ponga un año de fabricación válido");
+            Debug.Log("VTV no aprobada");
+
             return;
         }
 
         if (kilometros < 0)
         {
             Debug.Log("Ponga una cantidad de kilómetros válida");
+            Debug.Log("VTV no aprobada");
             return;
         }
 
         if (VTV > 2025 || VTV < modelo)
         {
             Debug.Log("Porfavor ingrese un vencimiento válido");
+            Debug.Log("VTV no aprobada");
+
             return;
         }
         if (HC < 5)
         {
             Debug.Log("Porfavor ingrese un HC válido");
+            Debug.Log("VTV no aprobada");
             return;
+        }
+
+        if (HC > 100)
+        {
+            Debug.Log("HC mayores a 100, VTV no aprobada");
+            return;
+        }
+
+        if (kilometros / (2025 - modelo) < 10000)
+        {
+            Debug.Log("VTV otorgada por 2 años");
+        }
+        if (kilometros / (2025 - modelo) > 10000)
+        {
+            Debug.Log("VTV otorgada por 1 año");
         }
     }
 
